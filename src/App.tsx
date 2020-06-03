3ccce86be7d338bb6e98508101cd18edd91eb4dc
GabrieleSuzart <gabrielesuzart@gmail.com>
2020-06-03 00:24:58 -0300

    Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => (
-  <h1>Go Barber</h1>
-)
+  <>
+    <GlobalStyle />
+    <h1>Go Barber</h1>
+  </>
+);
 
 export default App;
